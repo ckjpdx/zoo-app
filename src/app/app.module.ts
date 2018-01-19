d3943b6abb099d59c166adac9e362c66ccd28401
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-01-19 11:37:46 -0800

    add edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AddAnimalComponent } from './add-animal/add-animal.component';
 import { ListAnimalComponent } from './list-animal/list-animal.component';
+import { EditAnimalComponent } from './edit-animal/edit-animal.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AddAnimalComponent,
-    ListAnimalComponent
+    ListAnimalComponent,
+    EditAnimalComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/edit-animal/edit-animal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'edit-animal',
+  templateUrl: './edit-animal.component.html',
+  styleUrls: ['./edit-animal.component.css']
+})
+export class EditAnimalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
