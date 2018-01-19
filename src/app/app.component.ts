81ce08acf6d7c5cb4aae54cd2cf49ba0ba5dfe5c
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-01-19 08:40:43 -0800

    made constructor for animal class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,13 +22,6 @@ export class AppComponent {
   ]
 }
 export class Animal {
-  Species: string;
-  Name: string;
-  Age: number;
-  Diet: string;
-  Location: string;
-  Caretakers: number;
-  Sex: string;
-  Likes: string;
-  Dislikes: string;
+  constructor(
+    Species: string, Name: string, Age: number, Diet: string, Location: string, Caretakers: number, Sex: string, Likes: string, Dislikes: string) { }
 }
